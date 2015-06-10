4941aca642498591bb5a678c9a6331444813d3bd
Charlie Gillespie <usacfg@gmail.com>
2015-06-10 19:29:33 -0400

    all done

---
+++ cloneuser.rb
@@ -0,0 +1,15 @@
+require 'net/http'
+require 'json'
+require 'awesome_print'
+
+puts "Which Github user would you like to clone today?"
+print "User: "
+input = gets.strip
+github_repo_list_unparsed = Net::HTTP.get(URI("https://api.github.com/users/"+input+"/repos"))
+github_repo_list = JSON.parse(github_repo_list_unparsed)
+github_repo_list.each do |repo|
+	git_url = repo["git_url"]
+	system ("git clone "+git_url)
+end
+
+puts "All done. Cheers!"
---
+++ readme.md
@@ -0,0 +1,2 @@
+#Github Clone User
+A simple script to copy all of the repos that belong to a github user
